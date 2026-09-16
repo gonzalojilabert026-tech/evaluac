5a456217f7de2e40721979406da387e6f51f677d
gonzalo <gonzalojilabert026@gmail.com>
2026-09-16 18:19:53 -0300

    Completa vistas y plantillas de App2

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.inicio),
     path('informacion/', views.informacion),
+    path('app2/', include('app2.urls')),
 ]
