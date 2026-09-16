1eae2fe0b2a7ad9ab1e906837795e5c83eb08267
gonzalo <gonzalojilabert026@gmail.com>
2026-09-16 18:17:32 -0300

    Agrega vistas de inicio e información para App2

---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def inicio(request):
+    return render(request, 'app2/inicio.html')
+
+def informacion(request):
+    return render(request, 'app2/informacion.html')
