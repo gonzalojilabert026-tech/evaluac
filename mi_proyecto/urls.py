54a34d181be68a22983dfd3127c08335de8530c1
gonzalo <gonzalojilabert026@gmail.com>
2026-09-16 21:19:17 -0300

    Agrega proyecto Django App2

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from app1 import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio),
-    path('informacion/', views.informacion),
+    path('app2/', include('app2.urls')),
 ]
